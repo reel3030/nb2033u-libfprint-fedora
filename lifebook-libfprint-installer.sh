1be484bbd1bd7b68d01ddde7edad437e213bf0a4
HORIE <26223147+suikan4github@users.noreply.github.com>
2026-08-28 11:14:00 +0900

    Fedora RPM is successfuly build.
    At this moment, source code is official one.
    No NB-2033-U support.

--- lifebook-libfprint-installer.sh
+++ lifebook-libfprint-installer.sh
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 
-CONTAINER=work-container
+
 
 # Install fprintd and libfprint
 if [ "$OS" = "ubuntu" ]; then
@@ -40,15 +40,49 @@ if [ "$OS" = "ubuntu" ]; then
 
 elif [ "$OS" = "fedora" ]; then
     echo "Fedora detected."
+    CONTAINER=lifebook-work-container
+    BASEDIR=$(pwd)
+    LIBFPRINT=libfprint-1.94.100
+
+
     # Build is done inside container.
     toolbox create -c ${CONTAINER} -y
-    toolbox run -c ${CONTAINER} -- sudo dnf group install -y "development-tools"
     toolbox run -c ${CONTAINER} -- sudo dnf install -y \
-        meson ninja-build systemd-devel cmake \
-        libgusb-devel cairo-devel gobject-introspection-devel \
-        libgudev-devel gcc-c++
-    # These build tools is neede to install to host.
-    sudo dnf install -y ninja-build meson    
+        fedora-packager rpmdevtools dnf-plugins-core || \
+        { echo "Error: toolbox run 'sudo dnf install -y' failed." >&2; exit 1; }
+    # Create build directory
+    toolbox run -c ${CONTAINER} -- rpmdev-setuptree ||\
+        { echo "Error: toolbox run 'rpmdev-setuptree' failed." >&2; exit 1; }
+    # Download libfprint source RPM
+    toolbox run -c ${CONTAINER} -- dnf download --source $LIBFPRINT || \
+        { echo "Error: toolbox run 'dnf download --source libfprint' failed." >&2; exit 1; }
+    # Autoinstall dependency
+    toolbox run -c ${CONTAINER} -- sudo dnf builddep -y libfprint*.src.rpm || \
+        { echo "Error: toolbox run 'sudo dnf builddep -y libfprint*.src.rpm' failed." >&2; exit 1; }
+    # Expand SPRM ( SPECS and SOURCES under $(pwd)/rpmbuild/)
+    toolbox run -c ${CONTAINER} -- rpm -i --define "_topdir $BASEDIR/rpmbuild" libfprint*.src.rpm || \
+        { echo "Error: toolbox run 'rpm -i libfprint*.src.rpm' failed." >&2; exit 1; }
+    # Move to work dir
+    cd "$BASEDIR"/rpmbuild/SOURCES/ || \
+        { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SOURCES/'" >&2; exit 1; }
+    # Expand source code
+    tar -xf libfprint-*.tar.gz
+    cd libfprint-*/ || exit 1
+
+    # Modify source code here
+
+    # RPM spec files
+    # 1. リリース番号に独自ビルド識別子（.custom 等）を追加
+    # Release:        1.custom%{?dist}
+
+    # 2. Patch定義を追加（他のPatch項目の後などに追加）
+    # Patch999:       my-custom-fix.patch
+
+    # Build
+    cd "$BASEDIR"/rpmbuild/SPECS/ || \
+        { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SPECS/'" >&2; exit 1; }
+    toolbox run -c ${CONTAINER} -- rpmbuild --define "_topdir $BASEDIR/rpmbuild" -bb libfprint.spec || \
+        { echo "Error: toolbox run 'rpmbuild -bb libfprint.spec' failed." >&2; exit 1; }
 
 else
     echo "Unsupported operating system: $OS"
