3e54bcdb8ada2808e358cc8701d384493ba00bd6
HORIE <26223147+suikan4github@users.noreply.github.com>
2026-08-30 14:36:49 +0900

    Add conditional run to select :
    - Native source code.
    - Hacked source code to support NB-2033-U.

--- build.sh
+++ build.sh
@@ -42,25 +42,39 @@ toolbox run -c ${CONTAINER} -- rpm -i --define "_topdir $BASEDIR/rpmbuild" libfp
 # Move to work dir
 cd "$BASEDIR"/rpmbuild/SOURCES/ || \
     { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SOURCES/'" >&2; exit 1; }
-# Expand source code
-#tar -xf libfprint-*.tar.gz
 
-# Clone the development repo of Sebastian van de Meer
-git clone https://gitlab.freedesktop.org/Kernel-Error/libfprint.git || \
-    { echo "Error: Failed to clone S.V.D.Meer repo." >&2; exit 1; }
-cd libfprint || exit 1
+if  false ; then
+    # Expand source code
+    echo "Using the source code from the original source RPM."
+    tar -xf libfprint-*.tar.gz || \
+        { echo "Error: Failed to extract libfprint-*.tar.gz." >&2; exit 1; }
 
-# Checkout the newest commit at the point of 2026-7-31.
-# git checkout 344480a6d62a58f036680a81f5cf4f1e890afabb
-git checkout nb2033-support || \
-    { echo "Error: Failed to checkout nb2033-support branch." >&2; exit 1; }
+    cd "$BASEDIR"/rpmbuild/SPECS/ || \
+        { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SPECS/'" >&2; exit 1; }
+else
+    # If you want to use the source code from the development repo of Sebastian van de Meer, comment out the above line and uncomment the following line.
+    echo "Using the source code from the development repo of Sebastian van de Meer."
 
-cd "$BASEDIR"/rpmbuild/SPECS/ || \
-    { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SPECS/'" >&2; exit 1; }
+    # Clone the development repo of Sebastian van de Meer
+    git clone https://gitlab.freedesktop.org/Kernel-Error/libfprint.git || \
+        { echo "Error: Failed to clone S.V.D.Meer repo." >&2; exit 1; }
+    cd libfprint || exit 1
 
-# Modify release number. This will override the original version.
-sed -i -e "s/^Release.*$/Release:        1.99.nb2033u%{?dist}/" libfprint.spec || \
-    { echo "Error: Failed to modify the spec file." >&2; exit 1; }
+    # Checkout the newest commit at the point of 2026-7-31.
+    # git checkout 344480a6d62a58f036680a81f5cf4f1e890afabb
+    git checkout nb2033-support || \
+        { echo "Error: Failed to checkout nb2033-support branch." >&2; exit 1; }
+
+    cd "$BASEDIR"/rpmbuild/SPECS/ || \
+        { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SPECS/'" >&2; exit 1; }
+
+    # Modify release number. This will override the original version.
+    sed -i -e "s/^Release.*$/Release:        1.99.nb2033u%{?dist}/" libfprint.spec || \
+        { echo "Error: Failed to modify the spec file." >&2; exit 1; }
+
+fi
+
+
 
 # Build
 toolbox run -c ${CONTAINER} -- rpmbuild --define "_topdir $BASEDIR/rpmbuild" -bb libfprint.spec || \
