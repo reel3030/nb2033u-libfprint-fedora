2bd0328c5177e02e18972723bc640536fbec7671
HORIE <26223147+suikan4github@users.noreply.github.com>
2026-08-31 07:45:44 +0900

    Using patch.
    - Modify the build.sh script to use the correct path
    for the spec file and to add error handling for the sed commands.
    
    - Add patch.md file to document the changes made to the build.sh script.

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ toolbox run -c ${CONTAINER} -- rpm -i --define "_topdir $BASEDIR/rpmbuild" libfp
 cd "$BASEDIR"/rpmbuild/SOURCES/ || \
     { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SOURCES/'" >&2; exit 1; }
 
-if  false ; then
+if  true ; then
     # Expand source code
     echo "Using the source code from the original source RPM."
     tar -xf libfprint-*.tar.gz || \
@@ -55,26 +55,17 @@ else
     # If you want to use the source code from the development repo of Sebastian van de Meer, comment out the above line and uncomment the following line.
     echo "Using the source code from the development repo of Sebastian van de Meer."
 
-    # Clone the development repo of Sebastian van de Meer
-    git clone https://gitlab.freedesktop.org/Kernel-Error/libfprint.git || \
-        { echo "Error: Failed to clone S.V.D.Meer repo." >&2; exit 1; }
-    cd libfprint || exit 1
+    # Place the patch file in the SOURCES directory.
+    cp "$BASEDIR"/nb2033u.patch "$BASEDIR"/rpmbuild/SOURCES/ || \
+        { echo "Error: Failed to copy nb2033u.patch to ~/rpmbuild/SOURCES/." >&2; exit 1; }
 
-    # Checkout the newest commit at the point of 2026-7-31.
-    # git checkout 344480a6d62a58f036680a81f5cf4f1e890afabb
-    git checkout nb2033-support || \
-        { echo "Error: Failed to checkout nb2033-support branch." >&2; exit 1; }
-
-    cd "$BASEDIR"/rpmbuild/SPECS/ || \
-        { echo "Error: Failed to move to '$BASEDIR/rpmbuild/SPECS/'" >&2; exit 1; }
-
-    # Modify release number. This will override the original version.
-    sed -i -e "s/^Release.*$/Release:        1.99.nb2033u%{?dist}/" libfprint.spec || \
-        { echo "Error: Failed to modify the spec file." >&2; exit 1; }
+    # Modify release number and patch. This will override the original version.
+    sed -i -e "s/^Release.*$/Release:        1.99.nb2033u%{?dist}/" "$BASEDIR"/rpmbuild/SPECS/libfprint.spec || \
+        { echo "Error: Failed to modify the Release field of spec file." >&2; exit 1; }
+    sed -i '/^Source0:/a Patch0:         nb2033u.patch' "$BASEDIR"/rpmbuild/SPECS/libfprint.spec || \
+        { echo "Error: Failed to modify the Patch field ofspec file." >&2; exit 1; }
 
 fi
-
-
 
 # Build
 toolbox run -c ${CONTAINER} -- rpmbuild --define "_topdir $BASEDIR/rpmbuild" -bb libfprint.spec || \
---
+++ patch.md
@@ -0,0 +1,47 @@
+# nb2033u.patch
+
+This documents explain the `nb2033u.patch` file.
+
+Note that this patch is sticky to the libfprint version 1.94.100. If you want to use this patch on other versions, please check the source code and modify the patch file accordingly.
+
+# Purpose of nb2033u.patch
+
+This patch file is applied to the the source code of the libfprint.rpm. With this patch applied, the source code have support the NB-2033-U fingerprint reader.
+
+This patch method is official workflow of the RPM building. To make it happen, the `build.sh` script will do: 
+ 
+ 1. copy nb2033u.patch to the `rpmbuild/SOURCES` directory.
+ 2. Add the line `Patch0: nb2033u.patch` to the `libfprint.spec` file.
+
+# How nb2033u.patch was generated
+
+```sh
+# At first, define a function to generate the patch file.
+build_nb2033_patch() {
+    # Go to the work directory.
+    cd rpmbuild/SOURCES || return 1
+
+    # unzip the official source tree. 
+    tar -xzf libfprint-*.tar.gz || \
+        { echo "Error: Failed to extract libfprint-*.tar.gz." >&2; return 1; }
+
+    # Clone the development repo of Sebastian van de Meer
+    git clone https://gitlab.freedesktop.org/Kernel-Error/libfprint.git || \
+        { echo "Error: Failed to clone S.V.D.Meer repo." >&2; return 1; }
+    cd libfprint || return 1
+
+    # Checkout the newest commit at the point of 2026-7-31.
+    # This the support for the libfprint version 1.94.100.
+    git checkout 344480a6d62a58f036680a81f5cf4f1e890afabb || \
+        { echo "Error: Failed to checkout nb2033-support branch." >&2; return 1; }
+
+    cd .. || return 1
+
+    # Make patch
+    diff -uNr libfprint-v1.94.100 libfprint > nb2033u.patch
+}
+
+# Here we go.
+build_nb2033_patch
+
+```
